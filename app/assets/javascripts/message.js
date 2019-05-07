1b50b8513065341a2f4f61bd49602a8cd678f809
向川　英孝 <jinsino1987@gmail.com>
2019-05-07 19:32:09 +0900

    edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -34,12 +34,11 @@ $(document).on('turbolinks:load', function() {
       processData: false,
       contentType: false
     })
-    .done(function(data){
-      var html = buildHTML(data);
+    .done(function(message){
+      var html = buildHTML(message);
       $('.messages').append(html)
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('.form__message').val('')
-      $('.form__mask__image').val('')
       $('.form__submit').removeAttr('disabled');
     })
     .fail(function(){
