a4ec8b43d03c2a91d3ff560addda7cb7a040eb38
向川　英孝 <jinsino1987@gmail.com>
2019-05-07 18:25:52 +0900

    Create message hidouki

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,49 @@
+$(document).on('turbolinks:load', function() {
+  function buildHTML(message){
+    var img = ""
+    // console.log(message.image.url)
+    if (message.image.url != null) {
+      img = `<img src="${message.image.url}">`
+    }
+    var html = `<div class="message">
+                  <div class="upper-info">
+                    <p class="upper-info__user">
+                       ${message.user_name}
+                    </p>
+                    <p class="upper-info__date">
+                       ${message.strftime} 
+                    </p>
+                  </div>
+                  <div class="lower-info">
+                    <p class="message__text">
+                      ${message.content}
+                      ${ img }
+                    </p>
+                   </div>
+                </div>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('.form__message').val('')
+      $('.form__submit').removeAttr('disabled');
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
